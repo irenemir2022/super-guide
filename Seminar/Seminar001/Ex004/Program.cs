d9d11df7ec09f54b04cf0427ff2f6a2be29e01dc
Irina Mironova <irenemir2022@internet.ru>
2023-02-25 23:50:05 +0300

    добавили изменения в диаграмму и добавсили код к задаче 4

--- Seminar/Seminar001/Ex004/Program.cs
+++ Seminar/Seminar001/Ex004/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 4");
+Console.WriteLine("Введите число: ");
+int N = Convert.ToInt32(Console.Readline());
+int Startnumber = 1;
+
+  while (Startnumber <= N);
+    {
+      if (Startnumber %2 == 0)
+      Console.Write(N + ",");
+      Startnumber++;
+    }
+
