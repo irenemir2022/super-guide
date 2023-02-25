568310508aa40bd6072057494e6dc2e474ad5cc3
Irina Mironova <irenemir2022@internet.ru>
2023-02-25 19:32:31 +0300

    добавили второе число и условия когда одно число больше другого

--- Seminar/Seminar001/Ex001/Program.cs
+++ Seminar/Seminar001/Ex001/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.WriteLine("Задача 1");
 Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+  if (a > b)
+  {
+     Console.WriteLine(a + ">" + b);
+  }
+  else if (b > a)
+  {
+     Console.WriteLine(b + ">" + a);
+  }
