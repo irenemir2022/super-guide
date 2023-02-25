725c86884f42a4f04788c61dee6cb516e6fc4d05
Irina Mironova <irenemir2022@internet.ru>
2023-02-25 22:47:19 +0300

    добавили строки кода для задачи 3

---
+++ Seminar/Seminar001/Ex003/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача 3");
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+  if (a %2 == 0)
+  {
+    Console.WriteLine(a + "- Четное число: ");
+  }
+   else
+   {
+     Console.WriteLine(a + "- Нечетное число: ");
+   }
