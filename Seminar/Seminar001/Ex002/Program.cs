520f035385dce58bd6b760e0388c985c9f05da2d
Irina Mironova <irenemir2022@internet.ru>
2023-02-25 21:57:29 +0300

    внесли строчки кода с черновика в файл

--- Seminar/Seminar001/Ex002/Program.cs
+++ Seminar/Seminar001/Ex002/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Задача 2");
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+  if (a > b)
+  {
+    if (a > C)
+     {
+      Console.WriteLine("max" + a);
+     }
+    else
+       {
+        Console.WriteLine("max" + c);
+       }
+  }    
+       else if (b > c)
+       {
+        Console.WriteLine("max" + b);
+       }
+       else
+       {
+        Console.WriteLine ("max" + c);
+       }
