3988d42b027204751c540dbd239bd6171f21f17b
Irina Mironova <irenemir2022@internet.ru>
2023-02-26 01:05:15 +0300

    изменеия в коде

--- Seminar/Seminar001/Ex004/Program.cs
+++ Seminar/Seminar001/Ex004/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.WriteLine("Задача 4");
 Console.WriteLine("Введите число: ");
-int N = Convert.ToInt32(Console.Readline());
+int N = Convert.ToInt32(Console.ReadLine());
 int Startnumber = 1;
 
-  while (Startnumber <= N);
-    {
-      if (Startnumber %2 == 0)
-      Console.Write(N + ",");
-      Startnumber++;
-    }
+    while(Startnumber <= N)
+      { 
+        if (Startnumber %2 == 0)
+        Console.Write(Startnumber + ",");
+        Startnumber ++;
+      }
 
