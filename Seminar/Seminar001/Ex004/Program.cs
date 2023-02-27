f895ea2ce487838a7128ef52fc37b6994eb0c63e
Irina Mironova <irenemir2022@internet.ru>
2023-02-27 13:15:52 +0300

    исправили решение задачи 4, работает.

--- Seminar/Seminar001/Ex004/Program.cs
+++ Seminar/Seminar001/Ex004/Program.cs
@@ -1,12 +1,14 @@
 ﻿Console.WriteLine("Задача 4");
 Console.WriteLine("Введите число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int Startnumber = 1;
+int Startnumber= 1;
+ while(Startnumber <= N)
+  { 
+    if (Startnumber %2 == 0)
+     if (Startnumber == N || Startnumber == N - 1) Console.WriteLine(Startnumber);
+      else
+       Console.Write(Startnumber + ",");
+    Startnumber ++;
+  }
 
-    while(Startnumber <= N)
-      { 
-        if (Startnumber %2 == 0)
-        Console.Write(Startnumber + ",");
-        Startnumber ++;
-      }
-
+System.Console.WriteLine();
