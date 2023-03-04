3fd1194b7b62dd27e322caf1192119a6687d5f30
Irina Mironova <irenemir2022@internet.ru>
2023-03-04 16:36:23 +0300

    новая блок-схема в связи с правкой Ex002 c добавлением знака &&

---
+++ Seminar/Seminar002/Ex001/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a 3-digit number:");
+int Value = Convert.ToInt32(Console.ReadLine());
+int Length = Value.ToString().Length;
+if (Length == 3)
+ {
+	int result = ( Value/10 ) % 10;
+	Console.WriteLine("The second digit is " + result);
+ } 
+ else 
+ {
+	Console.WriteLine("Not a 3-digit number");
+ }
