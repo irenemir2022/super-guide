7b220847a289c80caa4ff7b9e193cc10b0de3c6c
Irina Mironova <irenemir2022@internet.ru>
2023-03-04 17:26:04 +0300

    добавили код решения задачи Ex003

--- Seminar/Seminar002/Ex003/Program.cs
+++ Seminar/Seminar002/Ex003/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a day of week");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day >= 1 && Day <=7) 
+{
+	if (Day >= 6) 
+	{
+		Console.Write("It's a holiday");
+	} 
+	else 
+	{
+		Console.Write("Working day");
+	}
+} 
+else 
+{
+	Console.Write("Enter a valid day between 1 and 7");
+}
