edc7cac68f4e3a372ddc146154d2da45d717456b
Irina Mironova <irenemir2022@internet.ru>
2023-03-07 22:19:50 +0300

    изменила решение задачи, использовала метод

--- Seminar/Seminar003/Ex001/Program.cs
+++ Seminar/Seminar003/Ex001/Program.cs
@@ -1,19 +1,21 @@
-﻿Console.WriteLine("Введите число: ");
-string number = Console.ReadLine();
-int length = number.Length;
+﻿Console.WriteLine("Задача 19");
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
 
-if (length == 5)
+void CheckingNumber(string number)//использовала метод
+                                  
 {
-    if (number[0] == number[4] && number[1] == number[3])
-    {
-        Console.WriteLine($"{number} - Палиндром");
-    }
-    else
-    {
-        Console.WriteLine($"{number} - НЕ палиндром");
-    }
+  if (number[0]==number[4] && number[1]==number[3])
+  {
+    Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
 }
-else
+
+if (number!.Length == 5)
 {
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+  CheckingNumber(number);
 }
+else 
+
+Console.WriteLine($"Введи правильное число");
