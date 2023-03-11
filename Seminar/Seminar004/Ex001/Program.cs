27b7796c3b0a36d739fa9182cbd5788b346ef244
Irina Mironova <irenemir2022@internet.ru>
2023-03-11 09:43:22 +0300

    добавили решение задачи

--- Seminar/Seminar004/Ex001/Program.cs
+++ Seminar/Seminar004/Ex001/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+// цикл возведения в степень
+for (int i = 1; i < b; i++) 
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
